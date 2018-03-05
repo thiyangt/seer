7aaa38bf9dbf643a3df313e3b9b2a98f4e6d8c1a
thiyangt <thiyanga.talagala@monash.edu>
2018-03-05 20:39:18 +1100

    included feature calculation functions

--- data-raw/yearly_exp2.R
+++
@@ -1,6 +0,0 @@
-library(readr)
-yearly_exp2 <- readr::read_rds("data-raw/yearly_exp2.rds")
-save(yearly_exp2, file='data/yearly_exp2.rdata', compress='xz')
-
-yearly_exp2_sub <- readr::read_rds("data-raw/yearly_exp2_sub.rds")
-save(yearly_exp2_sub, file='data/yearly_exp2_sub.rdata', compress='xz')
