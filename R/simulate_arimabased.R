75a57fb4c50c71e954624e02698e3cbd6ec2679c
thiyangt <thiyanga.talagala@monash.edu>
2018-03-31 10:21:38 +1100

    add simulate based on arima models function

---
+++ R/simulate_arimabased.R
@@ -0,0 +1,41 @@
+#' Simulate time series based on ARIMA models
+#'
+#' simulate multiple time series for a given series based on ARIMA models
+#' @param y a time series or M-competition data time series (Mcomp)
+#' @param Nsim number of time series to simulate
+#' @param Combine if TRUE, training and test data in the M-competition data are combined and generate a time
+#' series corresponds to the full length of the series. Otherwise, it generate a time series
+#' based on the training period of the series.
+#' @param M if TRUE, y is considered to be a Mcomp data object
+#' @return A list of time series.
+#' @author Thiyanga Talagala
+#' @import forecast
+#' @export
+sim_arimabased <- function(y, Nsim, Combine=TRUE, M=TRUE){
+    if (M ==TRUE){
+      if ("Combine"==TRUE){
+      train <- y$x
+      test <-  y$xx
+      combined <- ts.union(train, test)
+      combined <- pmin(combined[,1], combined[,2], na.rm = TRUE)
+      }else{
+      combined <- y$x}
+    }else{
+      combined <- y
+    }
+  fit <- auto.arima(combined)
+  length_series <- length(combined)
+  mat <- list()
+  for(i in 1:Nsim){
+    mat[[i]] <- simulate(fit, nsim=length_series, future=FALSE)}
+  return (mat)
+}
+#'@examples
+#'require(Mcomp)
+#'libray(seer)
+#'quaterly_m3 <- subset(M3, "yearly")
+#'sim_arimabased(quarterly_m3[[1]], 2, Combine=TRUE, M=TRUE)
+#'
+#'set.seed(1)
+#'tsy <- ts(rnorm(8), frequency=1)
+#'sim_arimabased(tsy, 2, Combine=FALSE, M=FALSE)
