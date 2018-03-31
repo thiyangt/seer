66bfdca5bfa8e1dfe777e03e2cb45b6aa8e7db02
thiyangt <thiyanga.talagala@monash.edu>
2018-03-31 16:44:38 +1100

    add cal_MASE function

---
+++ R/cal_MASE.R
@@ -0,0 +1,23 @@
+#'Mean Absolute Scaled Error(MASE)
+#'
+#'Calculation of mean absolute scaled error
+#'@param training training peiod of the time series
+#'@param test test period of the time series
+#'@param forecast forecast values of the series
+#'@param m frequency of the time series
+#'@return returns a single value
+#'@author Thiyanga Talagala
+#'@export
+cal_MASE <- function(training, test, forecast, m=1){
+
+ q_t <- abs(test-forecast)/mean(abs(diff(training, lag=m)))
+ return(mean(q_t))
+}
+#'example
+#'require(Mcomp)
+#'require(seer)
+#'require(magrittr)
+#'ts <- M3[[1]]$x
+#'fcast_arima <- auto.arima(ts) %>% forecast(h=6)
+#'cal_MASE(M3[[1]]$x, M3[[1]]$xx, fcast_arima$mean)
+
