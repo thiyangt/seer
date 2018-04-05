5e90656b3dae9e8305c3a085285956b23cee719f
thiyangt <thiyanga.talagala@monash.edu>
2018-04-06 04:18:13 +1000

    include accuracy measue calculation function

---
+++ R/accuracy_measures.R
@@ -0,0 +1,56 @@
+#' calculate forecast accuracy from different foecasting methods
+#'
+#' Calculate foecast accuracy on test set according to a specified citerion
+#' @param tslist a list of time series
+#' @param models a vector of models to compute
+#' @param database whether the time series is from mcomp or other
+#' @param accuracyFun function to calculate the accuracy measure, the arguments
+#' for the accuracy function should be training, test and forecast
+#' @param h forecast horizon
+#' @return a list with accuracy matrix, vector of arima models and vector of ets models
+#' @author Thiyanga Talagala
+#' @export
+fcast_accuracy <- function(tslist, models = c("ets", "arima", "rw", "rwd", "wn",
+                                         "theta", "stlar", "nn", "snaive", "mstl", "tbats"), database
+                           , accuracyFun, h){
+
+  arima_models <- NA
+  ets_models <- NA
+
+  if (database == "other") {
+   train_test <- lapply(tslist, function(temp){list(training=head_ts(temp,h), test=tail_ts(temp, h))})
+  } else {
+   train_test <- lapply(tslist, function(temp){list(training=temp$x, test=temp$xx)})
+  }
+
+  if ("arima"%in% models) {
+    arima_cal <- lapply(train_test, ARIMA, ACCURACY=cal_WA)
+    arima_models <- sapply(arima_cal, function(temp){temp$ARIMAmodel})
+  }
+
+  if ("ets"%in% models) {
+    ets_cal <- lapply(train_test, ETS, ACCURACY=cal_WA)
+    ets_models <- sapply(ets_cal, function(temp){temp$ETSmodel})
+  }
+
+
+ mat <- sapply(models, function(f){
+
+   switch(f,
+          arima = sapply(arima_cal, function(temp){temp$ARIMAaccuracy}),
+          ets = sapply(ets_cal, function(temp){temp$ETSaccuracy}),
+          rw = sapply(train_test, RW, ACCURACY=accuracyFun),
+          rwd = sapply(train_test, RWD, ACCURACY=accuracyFun),
+          wn = sapply(train_test, WN, ACCURACY=accuracyFun),
+          theta = sapply(train_test, THETA, ACCURACY=accuracyFun),
+          stlar = sapply(train_test, STLAR, ACCURACY=accuracyFun),
+          nn = sapply(train_test, NN, ACCURACY=accuracyFun),
+          snaive = sapply(train_test, SNAIVE, ACCURACY=accuracyFun),
+          mstl = sapply(train_test, MSTL, ACCURACY=accuracyFun),
+          tbats = sapply(train_test, TBATS, ACCURACY=accuracyFun)
+   )
+ })
+
+ return(list(accuracy=mat, ARIMA = arima_models, ETS =ets_models))
+
+}
---
+++ R/modelfitting.R
@@ -0,0 +1,142 @@
+# class labels
+
+# ETS
+ETS <- function(ts_info, ACCURACY){
+training <- ts_info$training
+test <- ts_info$test
+h <- length(test)
+ets_fit <- ets(training)
+forecastETS <- forecast(ets_fit,h)$mean
+ETSaccuracy <- ACCURACY(forecast=forecastETS,test=test, training=training)
+ETSmodel <- as.character(ets_fit)
+return(list(ETSmodel=ETSmodel, ETSaccuracy=ETSaccuracy))
+}
+
+
+# ARIMA
+ARIMA <- function(ts_info, ACCURACY){
+training <- ts_info$training
+test <- ts_info$test
+h <- length(test)
+arima_fit <- auto.arima(training)
+forecastARIMA <- forecast(arima_fit,h)$mean
+ARIMAaccuracy <- ACCURACY(forecast=forecastARIMA, test=test, training=training)
+ARIMAmodel <- as.character(arima_fit)
+return(list(ARIMAmodel=ARIMAmodel, ARIMAaccuracy=ARIMAaccuracy))
+}
+
+
+# Random Walk
+RW <- function(ts_info, ACCURACY){
+training <- ts_info$training
+test <- ts_info$test
+h <- length(test)
+rw_fit <- rwf(training,drift=FALSE, h=h)
+forecastRW <- forecast(rw_fit)$mean
+RWaccuracy <- ACCURACY(forecast=forecastRW,test=test, training=training)
+return(RWaccuracy)
+
+}
+
+#Random walk with drift
+
+RWD <- function(ts_info, ACCURACY){
+training <- ts_info$training
+test <- ts_info$test
+h <- length(test)
+rwd_fit <- rwf(training,drift=TRUE, h=h)
+forecastRWD <- forecast(rwd_fit)$mean
+RWDaccuracy <- ACCURACY(forecast=forecastRWD, test=test, training=training)
+return(RWDaccuracy)
+
+}
+
+# White noise process
+
+WN <- function(ts_info, ACCURACY){
+training <- ts_info$training
+test <- ts_info$test
+h <- length(test)
+fit_WN <- Arima(training,order=c(0,0,0))
+forecastWN <- forecast(fit_WN,h)$mean
+WNaccuracy <- ACCURACY(forecast=forecastWN,test=test, training=training)
+return(WNaccuracy)
+
+}
+
+
+# Theta Method
+THETA <- function(ts_info, ACCURACY){
+training <- ts_info$training
+test <- ts_info$test
+h <- length(test)
+m <- frequency(training)
+if (m > 1){
+  # using stheta method with seasonal adjustment
+  # require(forecTheta)
+  forecastTheta <- forecTheta::stheta(training,h=h, s='additive')$mean
+  THETAaccuracy <- ACCUARACY(forecast=forecastTheta, test=test, training=training)
+} else {
+  # using thetaf method
+  forecastTheta <-forecast::thetaf(training,h=length(test))$mean
+  THETAaccuracy <- ACCURACY(forecast=forecastTheta, test=test, training=training)
+}
+return(THETAaccuracy)
+}
+
+# STL-AR
+
+STLAR <- function(ts_info, ACCURACY){
+training <- ts_info$training
+test <- ts_info$test
+h <- length(test)
+forecastSTLAR <- stlar(training,h=h)$mean
+STLARaccuracy <- ACCURACY(forecast=forecastSTLAR, test=test, training=training)
+return(STLARaccuracy)
+}
+
+# Neural Network Time Series Forecasts
+NN <- function(ts_info, ACCURACY){
+training <- ts_info$training
+test <- ts_info$test
+h <- length(test)
+fit_nnetar <- nnetar(training)
+forecastnnetar <- forecast(fit_nnetar, h=h)$mean
+nnetarACCURACY <- ACCURACY(forecast=forecastnnetar, test=test, training=training)
+return(nnetarACCURACY)
+}
+
+
+# season naive method
+SNAIVE <- function(ts_info, ACCURACY){
+training <- ts_info$training
+test <- ts_info$test
+h <- length(test)
+forecastSNAIVE <- snaive(training, h=length(test))$mean
+SNAIVEaccuracy <- ACCURACY(forecast=forecastSNAIVE, test=test, training=training)
+return(SNAIVEaccuracy)
+}
+
+
+# mstl
+MSTL <- function(ts_info, ACCURACY){
+training <- ts_info$training
+test <- ts_info$test
+h <- length(test)
+fit_mstl <- mstl(training)
+forecastMSTL <- forecast(training, h=length(test))$mean
+MSTLaccuracy <- ACCURACY(forecast=forecastMSTL, test=test, training=training)
+return(MSTLaccuracy)
+}
+
+# TBATS
+
+TBATS <- function(ts_info, ACCURACY){
+training <- ts_info$training
+test <- ts_info$test
+h <- length(test)
+fitTBAT <- tbats(training)
+forecastTBATS <- forecast(fitTBAT, h=h)$mean
+TBATSaccuracy <- ACCURACY(forecast=forecastTBATS, test=test, training=training)
+return(TBATSaccuracy)
+}
