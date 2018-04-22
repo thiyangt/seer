41c8ce05f8971c2254edbfb2de40ec3643ad9aed
thiyangt <thiyanga.talagala@monash.edu>
2018-04-22 22:33:26 +1000

    add build_rf

---
+++ R/build_rf.R
@@ -0,0 +1,31 @@
+#' build random forest classifier
+#'
+#' train a random forest model and predict forecast-models for new series
+#' @param training_set data frame of features and class labels
+#' @param testset features of new time series
+#' @param ntree number of trees in the forest
+#' @param rf_type whether ru(random forest based on unbiased sample) or rcp(random
+#' forest based on class priors)
+#' @return a list containing the random forest and forecast-models for new series
+#' @export
+build_rf <- function(training_set, testset, rf_type=c("ru", "rcp"), ntree){
+
+  training_set$classlabels <- as.factor(training_set$classlabels)
+  # extract training columns
+  # random forest based on unbiased sample
+  if (rf_type=="ru"){
+    rf <- randomForest::randomForest(classlabels~ .,
+          data = training_set,importance = TRUE, ntree=ntree, mtry=8)
+  }
+  # random forest based on class priors
+  if (rf_type=="rcp"){
+    classWT <- 1/as.vector(table(training_set$classlabels))
+    rf <- randomForest::randomForest(classlabels~ .,
+          data = training_set,importance = TRUE, ntree=ntree, mtry=8,
+          classwt=classWT)
+  }
+
+  predictions <- predict(rf, testset)
+  out <- list(randomforest=rf, predictions=predictions)
+  return(out)
+}
