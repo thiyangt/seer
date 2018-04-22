57f6c164ac3005756e4230131f0a52db16ad5f58
thiyangt <thiyanga.talagala@monash.edu>
2018-04-23 09:45:34 +1000

    tweaks to build_rf function to pass a seed value as a parameter

--- R/build_rf.R
+++ R/build_rf.R
@@ -6,20 +6,23 @@
 #' @param ntree number of trees in the forest
 #' @param rf_type whether ru(random forest based on unbiased sample) or rcp(random
 #' forest based on class priors)
+#' @param seed a vlue for seed
 #' @return a list containing the random forest and forecast-models for new series
 #' @export
-build_rf <- function(training_set, testset, rf_type=c("ru", "rcp"), ntree){
+build_rf <- function(training_set, testset, rf_type=c("ru", "rcp"), ntree, seed){
 
   training_set$classlabels <- as.factor(training_set$classlabels)
   # extract training columns
   # random forest based on unbiased sample
   if (rf_type=="ru"){
+    set.seed(seed)
     rf <- randomForest::randomForest(classlabels~ .,
           data = training_set,importance = TRUE, ntree=ntree, mtry=8)
   }
   # random forest based on class priors
   if (rf_type=="rcp"){
     classWT <- 1/as.vector(table(training_set$classlabels))
+    set.seed(seed)
     rf <- randomForest::randomForest(classlabels~ .,
           data = training_set,importance = TRUE, ntree=ntree, mtry=8,
           classwt=classWT)
