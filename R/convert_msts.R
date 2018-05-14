a71646ea28b758b798d1fa726b21d8a83be880a0
thiyangt <thiyanga.talagala@monash.edu>
2018-05-14 11:36:39 +1000

    add convert_msts function

---
+++ R/convert_msts.R
@@ -0,0 +1,34 @@
+#' Convert multiple frequency time series into msts object
+#'
+#' Convert multiple frequency(daily, hourly, half-hourly, minutes, seconds)
+#' time series into msts object.
+#' @param y univariate time series
+#' @param category frequency data have been collected
+#' @return a ts object or msts object
+#' @export
+convert_msts <- function(y, category){
+switch(category,
+daily={
+    if( length(y) <= 366){
+      y <- ts(y, frequency = 7)
+    } else {
+      y <- forecast::msts(y, seasonal.periods=c(7, 365.25))
+    }
+},
+hourly={
+  if (length(y) > 8766){
+    y <- forecast::msts(y, seasonal.periods=c(24, 168, 8766))
+  } else if( 168 < length(y) & length(y) <= 8766) {
+    y <- forecast::msts(y, seasonal.periods=c(24, 168))
+  } else {
+    y <- ts(y, frequency=24)
+  }
+}
+)
+return(y)
+}
+#'@examples
+#'library(seer)
+#'data(M4)
+#'M4_daily <- subset(M4, "daily")
+#'length(M4_daily[[1]]$x)
