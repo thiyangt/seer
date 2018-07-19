539163e06a83a6a452f11802fae6169986ecfbfe
thiyangt <thiyanga.talagala@monash.edu>
2018-07-19 12:46:41 +1000

    updated hexsticker

--- logo/hexsticker.R
+++ logo/hexsticker.R
@@ -1,8 +1,7 @@
 if (!require('hexSticker')) install.packages('hexSticker')
 library(hexSticker)
-sticker("hexsticker/navy.png", package="seer",
-        s_x = 1.07, s_y = 0.5, s_width=.1, s_height=.1, p_y = 1.1,
-        p_size = 48,
+sticker("logo/navy.png", package="seer",
+        s_x = 1, s_y = 0.75, s_width=.43,
+        p_size = 31,
         h_color="mediumspringgreen", h_fill="navy", p_color = "mediumspringgreen",
         filename="logo/seer.png")
-
