38ab4377e278d1ebdaf28475c36a175f65a7512a
thiyangt <thiyanga.talagala@monash.edu>
2019-03-05 17:34:12 +0800

    fix length conditions in convert_msts

--- R/convert_msts.R
+++ R/convert_msts.R
@@ -9,16 +9,16 @@
 convert_msts <- function(y, category){
 switch(category,
 daily={
-    if( length(y) <= 366){
+    if( length(y) <= 2*366){
       y <- ts(y, frequency = 7)
     } else {
       y <- forecast::msts(y, seasonal.periods=c(7, 365.25))
     }
 },
 hourly={
-  if (length(y) > 8766){
+  if (length(y) > 2*8766){
     y <- forecast::msts(y, seasonal.periods=c(24, 168, 8766))
-  } else if( 168 < length(y) & length(y) <= 8766) {
+  } else if( 2*168 < length(y) & length(y) <= 2*8766) {
     y <- forecast::msts(y, seasonal.periods=c(24, 168))
   } else {
     y <- ts(y, frequency=24)
